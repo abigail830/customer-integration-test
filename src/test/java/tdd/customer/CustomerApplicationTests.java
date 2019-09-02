9c98c9c916891ff0069d5c06ae934cbe8db2e5b8
SaraQian <abigail830@163.com>
2019-09-02 10:19:26 +0800

    minor refactor and update readme

--- src/test/java/tdd/customer/CustomerApplicationTests.java
+++ src/test/java/tdd/customer/CustomerApplicationTests.java
@@ -1,9 +1,13 @@
 package tdd.customer;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-
-public class CustomerApplicationTests extends IntegrationTestBase{
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerApplicationTests{
 
 	@Test
 	public void contextLoads() {
